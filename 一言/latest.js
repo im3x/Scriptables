473b636611224e91d0954252293768f742533960
im3x-dev <65442933+im3x-dev@users.noreply.github.com>
2020-10-09 23:27:45 +0800

    Create latest.js

---
+++ 一言/latest.js
@@ -0,0 +1,52 @@
+// 一言
+// iOS14桌面组件
+// 运行于 scriptable
+// 编写于 20201004
+// 作者是 hack_fish
+// https://github.com/im3x/Scriptables
+// 更多脚本和精彩教程请关注作者公众号 {古人云}
+
+
+main()
+
+async function main() {
+  let data = await getData()
+  let widget = await createWidget(data)
+  if (config.runsInWidget) {
+    Script.setWidget(widget)
+  } else {
+    widget.presentLarge()
+  }
+  Script.complete()
+}
+
+
+// 创建组件
+async function createWidget(text) {
+  let w = new ListWidget()
+  w.backgroundColor = new Color("#2193B0", 1)
+
+  let title = w.addText("{ 一言 }")
+  title.font = Font.boldMonospacedSystemFont(16)
+  title.textColor = Color.white()
+
+  w.addSpacer(18)
+
+  let body = w.addText(text)
+  body.font = Font.lightMonospacedSystemFont(14)
+  body.textColor = Color.white()
+  body.textOpacity = 0.88
+
+  return w
+}
+
+// 获取数据
+async function getData() {
+  let t = new Request('https://v1.hitokoto.cn/?encode=text')
+  return t.loadString()
+}
+
+async function getImage (url) {
+  let r = new Request(url)
+  return await r.loadImage()
+}
