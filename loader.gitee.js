7c0adf948c03f56aec72864d6e2ef4ae9125a506
im3x-dev <65442933+im3x-dev@users.noreply.github.com>
2020-10-09 20:20:57 +0800

    Create loader.gitee.js

---
+++ loader.gitee.js
@@ -0,0 +1,32 @@
+//
+// scriptable 加载器（gitee版，适用于国内）
+// 用于加载远程 scriptable 桌面组件插件
+// author@im3x
+// 公众号@古人云
+//
+
+// 解析 & 替换桌面组件传递过来的参数，比如 welcome@latest:hello
+const PLUGIN = {
+  name: "welcome",
+  args: "hello",
+  version: "latest"
+}
+if (args.widgetParameter) {
+  let _args = args.widgetParameter.split(":")
+  let _plug = _args[0].split("@")
+  if (_plug.length === 2) {
+    PLUGIN["version"] = _plug[1]
+  } else {
+    PLUGIN["version"] = "latest"
+  }
+  PLUGIN["name"] = _plug[0]
+  PLUGIN["args"] = _args[1]
+}
+_load()
+async function _load () {
+  // 加载远程插件代码
+  const _REMOTE_CODE_REQ = new Request("https://gitee.com/im3x/Scriptables/raw/main/" + PLUGIN["name"] + "/" + PLUGIN["version"] + ".js?_=" + (+new Date))
+  const _REMOTE_CODE_JS = await _REMOTE_CODE_REQ.loadString()
+  // 执行代码
+  eval(_REMOTE_CODE_JS)
+}
