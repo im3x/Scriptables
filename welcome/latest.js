4d168dad5e7c2550e4cec199f91de512ac4c54c1
im3x-dev <65442933+im3x-dev@users.noreply.github.com>
2020-10-09 19:05:19 +0800

    Update latest.js

--- welcome/latest.js
+++ welcome/latest.js
@@ -1,5 +1,6 @@
 widget = new ListWidget()
 widget.addText("Hello, Scriptable :)")
+widget.url = 'https://github.com/im3x/Scriptables'
 if (config.runsInWidget) {
   Script.setWidget(widget)
 } else {
