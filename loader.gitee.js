81aeb777607b83b552caf4a2c28cca3ebf434f2c
im3x-dev <65442933+im3x-dev@users.noreply.github.com>
2020-10-10 18:37:32 +0800

    Update loader.gitee.js

--- loader.gitee.js
+++ loader.gitee.js
@@ -4,12 +4,13 @@
 // 用于加载远程 scriptable 桌面组件插件
 // author@im3x
 // 公众号@古人云
+// https://github.com/im3x/Scriptables
 //
 
 class Im3xLoader {
-  constructor () {
+  constructor (git = 'github') {
     // 仓库源
-    this.git = "gitee"
+    this.git = git
     // 解析参数
     this.opt = {
       name: 'welcome',
@@ -33,27 +34,52 @@ class Im3xLoader {
   async init () {
     // 判断文件是否存在
     let rendered = false
+    let widget
     if (FileManager.local().fileExists(this.filepath)) {
       try {
-        await this.render()
+        widget = await this.render()
         rendered = true
       } catch(e){}
     }
     // 加载代码，存储
     let req = new Request(`https://${this.git}.com/im3x/Scriptables/raw/main/${encodeURIComponent(this.opt['name'])}/${this.opt['version']}.js?_=${+new Date}`)
     let res = await req.loadString()
-    console.log(res)
-    await FileManager.local().writeString(this.filepath, res)
-    if (!rendered) await this.render()
+    // 如果404
+    if (req.response['statusCode'] === 404) {
+      widget = await this.render404()
+    } else {
+      await FileManager.local().writeString(this.filepath, res)
+      if (!rendered) {
+        widget = this.render()
+      }
+    }
 
-    Script.complete()
+    return widget
+
+  }
+  async render404 () {
+    let m = new ListWidget()
+    let t1 = m.addText("⚠️")
+    t1.centerAlignText()
+    m.addSpacer(10)
+    let t = m.addText("404")
+    t.textColor = Color.red()
+    t.centerAlignText()
+    m.url = 'https://github.com/im3x/Scriptables'
+    return m
   }
   async render () {
     let M = importModule(this.filename)
     let m = new M(this.opt['args'])
-    if (!config.runsInWidget) return await m.test()
     let w = await m.render()
-    Script.setWidget(w)
+    return w
   }
 }
-new Im3xLoader().init()
+const Loader = new Im3xLoader('gitee')
+const widget = await Loader.init()
+if (!config.runsInWidget) {
+  await widget.presentSmall()
+} else {
+  Script.setWidget(widget)
+}
+Script.complete()
