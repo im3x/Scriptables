bedb7c5ccbbf5e24d9fb94c6c2d2dc84953c805f
im3x-dev <65442933+im3x-dev@users.noreply.github.com>
2020-10-10 16:08:19 +0800

    Update loader.gitee.js

--- loader.gitee.js
+++ loader.gitee.js
@@ -22,11 +22,35 @@ if (args.widgetParameter) {
   Script.im3x["name"] = _plug[0]
   if (_args.length === 2) Script.im3x["args"] = _args[1]
 }
-_load()
-async function _load () {
+
+// 先读取本地是否有缓存代码，如果有，则直接导入
+const PATH = FileManager.local().documentsDirectory()
+const FILE_NAME = Script.im3x['name'] + '@' + Script['im3x']['version'] + '.js.im3x'
+const FILE = PATH + "/" + FILE_NAME
+if (FileManager.local().fileExists(FILE)) {
+  _init(FILE_NAME)
+  _load(FILE)
+} else {
+  await _load(FILE)
+  _init(FILE_NAME)
+}
+async function _load (FILE) {
   // 加载远程插件代码
+  console.log('load.start')
+  console.log(FILE)
   const _REMOTE_CODE_REQ = new Request("https://gitee.com/im3x/Scriptables/raw/main/" + encodeURIComponent(Script.im3x["name"]) + "/" + Script.im3x["version"] + ".js?_=" + (+new Date))
   const _REMOTE_CODE_JS = await _REMOTE_CODE_REQ.loadString()
-  // 执行代码
-  eval(_REMOTE_CODE_JS)
+  console.log('load.end')
+  console.log('write.start')
+  FileManager.local().writeString(FILE, _REMOTE_CODE_JS)
+  console.log('write.end')
+  return _REMOTE_CODE_JS
+}
+async function _init (FILE) {
+  let M = importModule(FILE)
+  let m = new M(Script.im3x['args'])
+  let w = await m.render()
+  if (!config.runsInWidget) return
+  Script.setWidget(w)
+  Script.complete()
 }
