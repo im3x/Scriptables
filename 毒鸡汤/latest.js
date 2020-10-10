5e5d33be5a6e5ba4e5272eb86ce26358c3105822
im3x-dev <65442933+im3x-dev@users.noreply.github.com>
2020-10-10 16:05:26 +0800

    Update latest.js

--- 毒鸡汤/latest.js
+++ 毒鸡汤/latest.js
@@ -6,40 +6,46 @@
 // https://github.com/im3x/Scriptables
 // 更多脚本和精彩教程请关注作者公众号 {古人云}
 
-main()
-
-async function main() {
-  let data = await getData()
-  let widget = await createWidget(data)
-  if (config.runsInWidget) {
-    Script.setWidget(widget)
-  } else {
-    widget.presentLarge()
+class Im3xWidget {
+  constructor () {}
+  async render () {
+    let text = await this._loadData()
+    let w = new ListWidget()
+    w.backgroundColor = new Color("#0eb83a", 1)
+  
+    let title = w.addText("{ 毒鸡汤  }")
+    title.font = Font.boldMonospacedSystemFont(16)
+    title.textColor = Color.white()
+  
+    w.addSpacer(18)
+  
+    let body = w.addText(text)
+    body.font = Font.lightMonospacedSystemFont(14)
+    body.textColor = Color.white()
+    body.textOpacity = 0.88
+  
+    return w
   }
-  Script.complete()
+  async _loadData () {
+    let req = new Request('https://api.qinor.cn/soup/')
+    let data = await req.loadString()
+    return data
+  }
+  // 用于测试
+  async test () {
+    if (config.runsInWidget) return
+    let widget = await this.render()
+    widget.presentSmall()
+  }
+  // 用户单独使用
+  async init () {
+    if (!config.runsInWidget) return
+    let widget = await this.render()
+    Script.setWidget(widget)
+    Script.complete()
+  }
 }
 
-// 创建组件
-async function createWidget(text) {
-  let w = new ListWidget()
-  w.backgroundColor = new Color(Device.isUsingDarkAppearance()?"#493131":"#0eb83a", 1)
-
-  let title = w.addText("{ 毒鸡汤 }")
-  title.font = Font.boldMonospacedSystemFont(16)
-  title.textColor = Color.white()
-
-  w.addSpacer(18)
-
-  let body = w.addText(text)
-  body.font = Font.lightMonospacedSystemFont(14)
-  body.textColor = Color.white()
-  body.textOpacity = 0.88
-
-  return w
-}
-
-// 获取数据
-async function getData() {
-  let t = new Request('https://api.qinor.cn/soup/')
-  return t.loadString()
-}
+module.exports = Im3xWidget
+new Im3xWidget().test()
+new Im3xWidget().init()
