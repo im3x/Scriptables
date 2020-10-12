5155432bd02fdbfd0d443cdbc7bd4ac2b42b8245
im3x-dev <65442933+im3x-dev@users.noreply.github.com>
2020-10-13 02:48:33 +0800

    Update loader.gitee.js
    增加组件操作函数支持

--- loader.gitee.js
+++ loader.gitee.js
@@ -78,6 +78,10 @@ class Im3xLoader {
   async render () {
     let M = importModule(this.filename)
     let m = new M(this.opt['args'])
+    if (!config.runsInWidget && m['runActions']) {
+      await m.runActions()
+      return false
+    }
     let w = await m.render()
     return w
   }
@@ -123,11 +127,9 @@ class Im3xLoader {
     Keychain.set(key, commit['sha'])
   }
 }
-const Loader = new Im3xLoader('gitee')
+const Loader = new Im3xLoader()
 const widget = await Loader.init()
-if (!config.runsInWidget) {
-  await widget.presentSmall()
-} else {
+if (config.runsInWidget && widget) {
   Script.setWidget(widget)
 }
 Script.complete()
