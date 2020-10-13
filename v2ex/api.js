3fd809d47f2a000c74f92d210fd9fbf824534975
im3x-dev <im3x@qq.com>
2020-10-13 17:56:38 +0800

    增加离线缓存和点击后应用内打开页面操作

--- v2ex/api.js
+++ v2ex/api.js
@@ -10,8 +10,10 @@
 
 class Im3xWidget {
   // 初始化，接收参数
-  constructor (arg) {
+  constructor (arg, loader) {
     this.arg = 'latest'
+    this.loader = loader
+    this.fileName = module.filename.split('Documents/')[1]
     this.widgetSize = config.widgetFamily
     if (arg === 'hot') this.arg = 'hot'
   }
@@ -29,7 +31,9 @@ class Im3xWidget {
     let w = new ListWidget()
     let data = await this.getData()
     let topic = data[0]
-    w.url = topic['url']
+    w.url = this.loader ? this.getURIScheme('open-url', {
+      url: topic['url']
+    }) : topic['url']
     w = await this.renderHeader(w)
     let content = w.addText(topic['title'])
     content.font = Font.lightSystemFont(16)
@@ -74,7 +78,9 @@ class Im3xWidget {
   }
   async renderCell (widget, topic) {
     let body = widget.addStack()
-    body.url = topic['url']
+    body.url = this.loader ? this.getURIScheme('open-url', {
+      url: topic['url']
+    }) : topic['url']
 
     let left = body.addStack()
     let avatar = left.addImage(await this.getImage(topic['member']['avatar_large'].replace('mini', 'large')))
@@ -108,23 +114,63 @@ class Im3xWidget {
     header.centerAlignContent()
     let icon = header.addImage(_icon)
     icon.imageSize = new Size(14, 14)
+    icon.cornerRadius = 4
     header.addSpacer(10)
     let title = header.addText(_title)
     if (customStyle) title.textColor = Color.white()
     title.textOpacity = 0.7
-    title.font = Font.boldSystemFont(14)
+    title.font = Font.boldSystemFont(12)
     
     widget.addSpacer(15)
     return widget
   }
+    /**
+   * 用户传递的组件自定义点击操作
+   */
+  async runActions () {
+    let { act, data } = this.parseQuery()
+    if (!act) return
+    if (act === 'open-url') {
+      Safari.openInApp(data['url'], false)
+    }
+  }
+
+  // 获取跳转自身 urlscheme
+  // w.url = this.getURIScheme("copy", "data-to-copy")
+  getURIScheme (act, data) {
+    let _raw = typeof data === 'object' ? JSON.stringify(data) : data
+    let _data = Data.fromString(_raw)
+    let _b64 = _data.toBase64String()
+    return `scriptable:///run?scriptName=${encodeURIComponent(Script.name())}&act=${act}&data=${_b64}&__widget__=${encodeURIComponent(args['widgetParameter'])}`
+  }
+  // 解析 urlscheme 参数
+  // { act: "copy", data: "copy" }
+  parseQuery () {
+    const { act, data } = args['queryParameters']
+    if (!act) return { act }
+    let _data = Data.fromBase64String(data)
+    let _raw = _data.toRawString()
+    let result = _raw
+    try {
+      result = JSON.parse(_raw)
+    } catch (e) {}
+    return {
+      act,
+      data: result
+    }
+  }
   // 获取远程图片
   async getImage (url) {
-    console.log('get-image')
-    console.log(url)
-    let req = new Request(url)
-    let img = await req.loadImage()
-    console.log('get.image.done')
-    return img
+    try {
+      let req = new Request(url)
+      return await req.loadImage()
+    } catch (e) {
+      let ctx = new DrawContext()
+      ctx.size = new Size(100, 100)
+      ctx.setFillColor(Color.red())
+      ctx.fillRect(new Rect(0, 0, 100, 100))
+      return await ctx.getImage()
+    }
   }
   // 给图片加透明遮罩
   async shadowImage (img) {
@@ -142,9 +188,30 @@ class Im3xWidget {
   // 加载数据
   async getData () {
     let api = `https://www.v2ex.com/api/topics/${this.arg}.json`
-    let req = new Request(api)
-    let res = await req.loadJSON()
-    return res
+    let data = await this.fetchAPI(api)
+    return data
+  }
+  async fetchAPI (api, json = true) {
+    let data = null
+    const cacheKey = `${this.fileName}_cache`
+    try {
+      let req = new Request(api)
+      data = await (json ? req.loadJSON() : req.loadString())
+    } catch (e) {}
+    // 判断数据是否为空（加载失败）
+    if (!data) {
+      // 判断是否有缓存
+      if (Keychain.contains(cacheKey)) {
+        let cache = Keychain.get(cacheKey)
+        return json ? JSON.parse(cache) : cache
+      } else {
+        // 刷新
+        return null
+      }
+    }
+    // 存储缓存
+    Keychain.set(cacheKey, json ? JSON.stringify(data) : data)
+    return data
   }
   // 用于测试
   async test () {
@@ -161,7 +228,7 @@ class Im3xWidget {
   }
   // 单独运行
   async init () {
-    if (!config.runsInWidget) return
+    if (!config.runsInWidget) return await this.runActions()
     let widget = await this.render()
     Script.setWidget(widget)
     Script.complete()
